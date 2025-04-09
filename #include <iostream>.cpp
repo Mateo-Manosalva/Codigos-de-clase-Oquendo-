f08cdd02e278fe99cea51e3d1d4561c2f26a507d
ymanosalvc <ymanosalvac@unal.edu.co>
2025-04-08 21:26:12 -0500

    Archivos

---
+++ #include <iostream>.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(void)  
+{
+    std::string nombre;
+    std::cout <<"Introduce tu nombre";
+    std::getline (std::cin, nombre)
+    std::cout << "Hola," <<nombre\n
+    return 0
+}
