78d6b6a64ac5978d8d3450d38c408d289269c3ce
ymanosalvc <ymanosalvac@unal.edu.co>
2025-04-09 07:28:18 -0500

    Clase 2025 04 09 Primer codigo adivinanza

---
+++ guess_game.cpp
@@ -0,0 +1,17 @@
+/*
+Juego de adivinar un numero:
+Tener un numero
+Preguntar al usuario el numero
+Informar si lo adivino o no
+*/
+
+#include <iostream>
+
+int main(void) {
+    const int NUM = 10;
+    int guess;
+    std::cout << "Adivina el numero entre 1 y 100 \n";
+    std::cin >> guess; // Determina la variable proporcionada
+    
+    return 0;
+}
