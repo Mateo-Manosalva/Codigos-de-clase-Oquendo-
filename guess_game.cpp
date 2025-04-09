650d22b38da664abe095cf3432989b41f79fede9
ymanosalvc <ymanosalvac@unal.edu.co>
2025-04-09 07:43:08 -0500

    2025 04 09 Segunda version (if, else)

--- guess_game.cpp
+++ guess_game.cpp
@@ -12,6 +12,14 @@ int main(void) {
     int guess;
     std::cout << "Adivina el numero entre 1 y 100 \n";
     std::cin >> guess; // Determina la variable proporcionada
+
+    if (guess == NUM) { // Determina que si sucede una cosa hace x accion y sino realiza y accion
+    std::cout << "Correcto \n";
+    
+    }
+    else {
+    std::cout <<"incorrecto \n";
+    }
     
     return 0;
 }
